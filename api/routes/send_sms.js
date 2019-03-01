2765852f49b865b9203858ea301a6fbc5e1d925f
Priyanshu Nayan <priyanshunayan456@gmail.com>
2019-03-01 18:14:25 +0530

    bulk sms prone area

--- api/routes/send_sms.js
+++ api/routes/send_sms.js
@@ -1,29 +1,42 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
+const Users = require('../models/users');
 /* const unirest = require('unirest');
 const unirestReq = unirest('POST', "https://www.fast2sms.com/dev/bulk"); */
 /* const apiKey = 'SsIglq9BuhkHDOn7mNbc5wQxPKztF2f1UVdjLTC0eXJv4YMGAoXNVfebcJZAIgLiTlG7BFs8rPnUxRWv'; */
 const request = require('request');
 /* let url = "http://api.msg91.com/api/v2/sendsms";
 const smsApiKey = "241761AHj4Wys5oNu5bbb3ad5"; */
-let url = "https://api.bulksms.com/v1/messages";
-
+/* let url = "https://api.bulksms.com/v1/messages"; */
 router.post('/bulk', (req, res, next) => {
     console.log('inside bulk');
-    const sender = 'FSTSMS';
-    const message = req.body.message;
-    const language ='english';
-    const route = 'p';
-    const numbers = req.body.numbers;
-   // req.header('Authorization', 'Basic DBFB4890966C4A689E08C17E45325C2F-02-7');
- /*    request.get(`http://103.16.101.52:8080/sendsms/bulksms?username=adar-ezytech&password=sms12345&type=0&dlr=1&destination=8340326054,9827629560&source=EZYPAY&message=${message}`).on('response', (response) => {
-        res.status(200).json({
-            message: 'Message sent successfully',
-            response: response
-        })
-    }); */
-    request({
+    const pin= req.body.pin;
+    const message = req.body.message
+    let numberArray = [];
+    // Querying Database
+    Users.find({pin: pin}).exec((err, users) => {
+        if(err){
+            res.status(500).json({
+                message:"An error ocurred"
+            })
+        } else {
+            users.forEach(user => {
+                numberArray.push(user.phone);
+            })
+            let numberString = numberArray.join(',');
+            let url = `http://login.heightsconsultancy.com/API/WebSMS/Http/v1.0a/index.php?username=TravlP&password=password@123&sender=TravlP&to=${numberString}&message=${message}`;
+            //Making request final
+            request.get(url).on('response', (response) => {
+                res.status(200).json({
+                    message: "Message sent successsfuly",
+                    response : response
+                })
+            }) 
+        }
+    })
+
+/*     request({
         url: url,
         method: 'POST',
         headers:{
@@ -33,17 +46,11 @@ router.post('/bulk', (req, res, next) => {
             [
                 {
                   "from": {
-                    "type": "INTERNATIONAL",
                     "address": "8340326054"
                   },
                   "to": [
                     {
-                      "type": "INTERNATIONAL",
-                      "address": "8989943999",
-                      "fields": [
-                        "Jack",
-                        "$200.00"
-                      ]
+                      "address": "8989943999"
                     }
                   ],
                   "routingGroup": "ECONOMY",
@@ -66,7 +73,7 @@ router.post('/bulk', (req, res, next) => {
             "message": "Message was sent successfully",
             response: response
         })
-    })
+    }) */
 /*     request({
         url: url,
         method: 'POST',
